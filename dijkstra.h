8d791ba89fe9c1d83de095b3a0e0ad9bec3c9e9e
Sher Minn Chong <schong7@wisc.edu>
2012-12-08 17:16:55 -0600

    Add header files

---
+++ dijkstra.h
@@ -0,0 +1,19 @@
+#ifndef DIJKSTRA
+#define DIJKSTRA
+
+#include "linkstate.h"
+typedef struct __list_entry_t
+{
+	int cost;
+	host_t this;
+	host_t reached_from;
+
+}list_entry_t;
+
+typedef struct __list_t 
+{
+	int num_entries;
+	list_entry_t entries[MAX_NODES];
+}list_t;
+
+#endif
---
+++ linkstate.h
@@ -0,0 +1,33 @@
+#ifndef LINKSTATE
+#define LINKSTATE
+
+#define MAX_NODES 	20		//maximum number of nodes in the topology
+#define IP_LEN 		32 		//number of chars in an IP address in dot notation
+#include <stdbool.h>
+//single host in network
+typedef struct __host_t
+{
+	char ip[IP_LEN];
+	short port;
+	bool online;
+
+}host_t;
+
+//single entry in topology table for one node
+typedef struct __topology_entry_t
+{
+	host_t node;	//node this entry is for
+	host_t neighbours[MAX_NODES];
+	int cost;
+
+}topology_entry_t;
+
+//topology table struct
+typedef struct __topology_table_t
+{
+	int num_nodes;
+	topology_entry_t entries [MAX_NODES];
+
+}topology_table_t;
+
+#endif
