6817528be386f2e5fd47442c8d4bf2929bfb0cd1
Sher Minn Chong <schong7@wisc.edu>
2012-12-08 17:08:02 -0600

    Implement readtopology

--- emulator.c
+++ emulator.c
@@ -11,6 +11,7 @@
 #include <time.h>
 #include <sys/time.h>
 #include "util.h"
+#include "linkstate.h"
 
 #define MAX_LINE 512
 #define MAX_DELAYED_PACKETS 512
@@ -18,21 +19,19 @@
 int debug = 1;
 int PORT;
 int QUEUE_SIZE;
-char* LOG;
-char* TABLE_NAME = "table.txt";
+char* TOPOLOGY = "topology.txt";
 forwarding_entry_t* forwardingTable;
 int numTableEntries = 0;
 
-char* corArgText = "Please run the program with the correct arguments.\n\n-p:  The port number\n-q:  The queue size\n-f:  The file name\n-l:  The log file name\n";
+char* corArgText = "Please run the program with the correct arguments.\n\n-p:  The port number\nf:  The file name\n";
 char* corPortText = "Please choose a port number greater than 1024 and less than 65536.\n";
 
-queue_t* priority1;
-queue_t* priority2;
-queue_t* priority3;
+queue_t* queue;
 
 forward_info_t curr_delayed_packet;
-
 forward_info_t delaying_packets[MAX_DELAYED_PACKETS]; //array of packets being delayed
+
+topology_table_t table;
 
 int delaying = 0;
 
@@ -64,7 +63,7 @@ void checkArgs(int argc, char* argv[])
 	char* errorChk;
 	
 	/* Check for the correct number of arguments */
-	if (argc != 9)
+	if (argc != 5)
 	{
 		die(corArgText);
 	}
@@ -86,21 +85,10 @@ void checkArgs(int argc, char* argv[])
 				die(corPortText);
 			}
 		}
-		else if (strcmp(argType, "-q") == 0)
-		{
-			iArg = strtol(argv[i+1], &errorChk, 10);
-			checkArg(errorChk,argv[i+1], argType);
-			QUEUE_SIZE = iArg;
-		}
 		else if (strcmp(argType, "-f") == 0)
 		{
 			sArg = argv[i+1];
-			TABLE_NAME = sArg;
-		}
-		else if (strcmp(argType, "-l") == 0)
-		{
-			sArg = argv[i+1];
-			LOG = sArg;
+			TOPOLOGY = sArg;
 		}
 		else if ((i % 2) == 1)
 		{
@@ -109,6 +97,9 @@ void checkArgs(int argc, char* argv[])
 			die(corArgText);
 		}
 	}
+
+	//init variables
+	QUEUE_SIZE = 1;
 }
 
 double getTimeDiff(struct timeval start_time, struct timeval end_time)
@@ -148,176 +139,148 @@ get_IP(char* hostname)
 	return *ip;
 }
 
-int
-buildTable()
+FILE* 
+open_file(char* filename)
 {
-	// Get the IP address of 
-	char recv_name[MAX_LINE];
-	gethostname(recv_name, sizeof(recv_name));
-	int thisIP = get_IP(recv_name);
-
-	// Put the forwarding table into an array
-	// Open the forwarding table file for reading
-	FILE* fp = fopen(TABLE_NAME, "rt");
+	FILE* fp = fopen(filename, "rt");
 	if (fp == NULL)
 	{
-	if(debug)	printf("Could not open forwarding table file %s.\n\n", TABLE_NAME);
-		return 1;
+		return NULL;
+	}
+	return fp;
+}
+
+void
+make_host(char* h, host_t* host)
+{
+	char* saveptr_host;
+	char* host_ip 	= strtok_r(h, ",", &saveptr_host);
+	char* host_port	= strtok_r(NULL, "", &saveptr_host);
+
+	if(debug)
+		printf("%s, %s\n", host_ip, host_port);
+	memcpy(host->ip, host_ip, sizeof(host->ip));
+	host->port = atoi(host_port);
+}
+
+////----------------------------------------LINK STATE ROUTING------------------------------------////
+int
+read_topology()
+{
+	FILE* fp;
+	if((fp = open_file(TOPOLOGY)) == NULL)
+	{
+		if(debug)
+			printf("Topology table file cannot be opened\n");
+		return -1;
 	}
 
-	// Read the file to determine the number of lines
-	char line[MAX_LINE];
+	int num_nodes = 0;
+
+	char line[MAX_LINE]; //line buffer
+
+	//read file
 	while(fgets(line, sizeof(line), fp) != NULL)
 	{
-		numTableEntries++;
-		memset(&line[0], 0, sizeof(line));
-	}
-
-	// Set the file pointer back to the start of the file
-	fseek(fp, 0, SEEK_SET);
-
-	// Allocate space in the array for the entries
-	forwardingTable = malloc(numTableEntries * sizeof(forwarding_entry_t));
-
-	// Break up the data on each line and add it to the array
-	int index = 0;
-	struct hostent* host;
-	struct in_addr* hostAddr;
-	int* ipInTable;
-	while(fgets(line, sizeof(line), fp))
-	{
-		// Split up the line by spaces and put the data into a struct
-		// Emulator
-		if(debug) printf("%s\n", line);
-		char emuMachine[MAX_LINE];
-		strcpy(emuMachine, strtok(line, " "));
-		if(debug) printf("emuMachine: %s\n", emuMachine);
-		strcat(emuMachine, ".cs.wisc.edu");
-
-		host = gethostbyname(emuMachine);
-		hostAddr = (struct in_addr*)host->h_addr;
-		ipInTable = (int*)hostAddr;
-
-		forwardingTable[index].emuIP = *ipInTable;
-		forwardingTable[index].emuPort = (short)atoi(strtok(NULL, " "));
-
-		if(debug) printf("IP: %d\n", forwardingTable[index].emuIP);
-
-		//If this listing isn't for this emulator, continue to the next listing
-		if ((thisIP != *ipInTable) || (PORT != forwardingTable[index].emuPort))
-		{
-			memset(&line[0], 0, sizeof(line));
+		//ignore empyt lines
+		if(strlen(line) <= 1) 
 			continue;
+
+		if(debug)
+			printf("Line: %s\n", line);
+
+		topology_entry_t entry;
+
+		//read first host in entry
+		char* h;
+		char* saveptr_line;
+
+		if((h = strtok_r(line, " ", &saveptr_line)))
+		{
+			//grab the node of this entry
+			make_host(h, &(entry.node));
 		}
 
-		// Destination
-		char destMachine[MAX_LINE];
-		strcpy(destMachine, strtok(NULL, " "));
-		strcat(destMachine, ".cs.wisc.edu");
+		//grab neighbours and stuff them into entry
+		int num_neighbours = 0;
 
-		host = gethostbyname(destMachine);
-		hostAddr = (struct in_addr*)host->h_addr;
-		ipInTable = (int*)hostAddr;
+		while ((h = strtok_r(NULL, " ", &saveptr_line)) != NULL)
+		{
+			make_host(h, &(entry.neighbours[num_neighbours]));
+			num_neighbours++;
+		}
 
-		forwardingTable[index].destIP = *ipInTable;
-		forwardingTable[index].destPort = (short)atoi(strtok(NULL, " "));
+		//put entry in table
+		memcpy(&(table.entries[num_nodes]), &entry, sizeof(topology_entry_t));
+		num_nodes++;
+	}                             
 
-		// Next Hop
-		char nextHopMachine[MAX_LINE];
-		strcpy(nextHopMachine, strtok(NULL, " "));
-		strcat(nextHopMachine, ".cs.wisc.edu");
-
-		host = gethostbyname(nextHopMachine);
-		hostAddr = (struct in_addr*)host->h_addr;
-		ipInTable = (int*)hostAddr;
-
-		forwardingTable[index].nextHopIP = *ipInTable;
-		forwardingTable[index].nextHopPort = (short)atoi(strtok(NULL, " "));
-
-		// Delay
-		forwardingTable[index].delay = atoi(strtok(NULL, " "));
-
-		// Loss Probability
-		forwardingTable[index].lossProb = atoi(strtok(NULL, " "));
-
-		// Clear the array
-		memset(&line[0], 0, sizeof(line));
-		forwarding_entry_t a;
-		a = forwardingTable[index];
-		if(debug) printf("Entry #%d\nEmulator IP: %d,%d\nDest: %d,%d\nNext Hop: %d,%d\nDelay: %d\nLoss Prob: %d\n",index + 1, forwardingTable[index].emuIP,a.emuPort,a.destIP,a.destPort,a.nextHopIP,a.nextHopPort,a.delay,a.lossProb);
-		if(debug) printf("______________________________________________________________\n");
-		index++;
-	}
-	fclose(fp);
-
+	table.num_nodes = num_nodes;
+	if(debug)
+		printf("Built topology table\n");
 	return 0;
 }
 
-void logerror(fpacket_t packet, char* reason)
+void
+get_my_IP(char* ip)
 {
-	printf("Logged %s\n", reason);
-	//if(debug)	printf("Start logging\n");
-	fflush(stdout);
-	struct hostent *source;
-	struct hostent *dest;
-	int sourceIP;
-	short sourcePort;
-	int destIP;
-	short destPort;
-	char cPriority;
-	int iPriority;
-	int payloadSize;
-	
-	// Get info about source
-	sourceIP = packet.fheader.src_address;
-	sourcePort = packet.fheader.src_port;
-	if((source = gethostbyaddr(&sourceIP, sizeof(destIP), AF_INET)) == NULL)
-	{
-		die("Cannot get source info\n");
-	}
+	struct hostent* emulator_host;
+	struct sockaddr_in emulator;
 
-	// Get infor about destination
-	destIP = packet.fheader.dest_address;
-	destPort = packet.fheader.dest_port;
-	if((dest = gethostbyaddr(&destIP, sizeof(destIP), AF_INET)) == NULL)
-	{
-		die("Cannot get dest info\n");
-	}
+	char emulator_hostname[MAX_LINE];
+	gethostname(emulator_hostname, sizeof(emulator_hostname));
+	emulator_host = gethostbyname(emulator_hostname);
 
-	// Get the time of the loss to millisecond granularity
-	time_t t;
-	struct tm curTime;
-	struct timeval ms;
+	bcopy(emulator_host->h_addr, (char*) &emulator.sin_addr, emulator_host->h_length);
+	char* emulator_ip_dot_notation = inet_ntoa(emulator.sin_addr);
+	printf("My IP is %s\n", emulator_ip_dot_notation);
 
-	t = time(NULL);
-	curTime = *localtime(&t);
-	gettimeofday(&ms, NULL);
-
-	// Get the priority of the packet
-	memcpy(&cPriority, &(packet.fheader), sizeof(char));
-	iPriority = (int)cPriority;
-
-	// Get the length of the payload
-
-	payloadSize = packet.fheader.length;
-
-	FILE* fp;
-	fp = fopen(LOG, "a+");
-
-	if (fp == NULL)
-	{
-		die("Could not open log file.\n");
-		return;
-	}
-
-	fprintf(fp, "\n%s\nSource -- Name: %s, Port: %d\nDestination -- Name: %s, Port: %d\n", reason, source->h_name, sourcePort, dest->h_name, destPort);
-	fprintf(fp, "Time of loss: %d-%d-%d %d:%d:%d.%d\n",curTime.tm_year + 1900, curTime.tm_mon + 1, curTime.tm_mday, curTime.tm_hour, curTime.tm_min, curTime.tm_sec, (int)(ms.tv_usec * .001));
-	fprintf(fp, "Priority Level: %d\nSize of payload: %d\n", iPriority, payloadSize);
-	fprintf(fp, "_____________________________________________________________\n");
-
-	fflush(fp);
-	fclose(fp);
+	memcpy(ip, emulator_ip_dot_notation, sizeof(emulator_ip_dot_notation));
 }
+
+void
+make_list_entry(host* this, host* fromm list_entry_t* entry)
+{
+
+}
+
+int
+calc_shortest_path()
+{
+	// Get my IP in dot notation
+	char my_IP[MAX_LINE];
+	get_my_IP(my_IP);
+
+	list_t confirmed;
+
+	// 	1. Initialize the Confirmed list with an entry for myself; this entry has
+	//  a cost of 0.
+	confirmed.entries[0] = 
+
+	//  2. For the node just added to the Confirmed list in the previous step,
+	// call it node Next and select its LSP
+	// 
+	// 3. For each neighbor (Neighbor) of Next, calculate the cost (Cost) to
+	// reach this Neighbor as the sum of the cost from myself to Next and
+	// from Next to Neighbor.
+
+	// (a) If Neighbor is currently on neither the Confirmed nor the
+	// Tentative list, then add (Neighbor, Cost, NextHop) to the
+	// Tentative list, where NextHop is the direction I go to reach Next.
+
+	// (b) If Neighbor is currently on the Tentative list, and the Cost is less
+	// than the currently listed cost for Neighbor, then replace the
+	// current entry with (Neighbor, Cost, NextHop), where NextHop
+	// is the direction I go to reach Next.
+
+	// 4. If the Tentative list is empty, stop. Otherwise, pick the entry from
+	// the Tentative list with the lowthe lowest cost, move it to the Confirmed list,
+	// and return to step 2.
+	return 0;
+
+}
+////------------------------------------END LINK STATE ROUTING------------------------------------------////
+
 
 //------------------------------------------ START SHER MINN -----------------------------------------------------//
 //------------------------------------------ START SHER MINN -----------------------------------------------------//
@@ -325,7 +288,7 @@ void logerror(fpacket_t packet, char* reason)
 //------------------------------------------ START SHER MINN -----------------------------------------------------//
 
 //enqueues np 
-int queue(fpacket_t fp, forwarding_entry_t fe)
+int queue_packet(fpacket_t fp, forwarding_entry_t fe)
 {
 	//if(debug) printf("Start queueing\n");
 	fpacket_t* np = &fp;
@@ -340,48 +303,12 @@ int queue(fpacket_t fp, forwarding_entry_t fe)
 	f.prob_drop = fe.lossProb;
 	f.delay 	= fe.delay;
 
-	//check priority level and enqueue appropriately
-	if((np->fheader).priority == 1)
-	{
-		full = enqueue(priority1, &f);
-		//if(debug)
-		// {
-		// 	printf("Put packet in Queue 1\n");
-		// 	printf("Queue 1 size : %d\n", priority1->size);
-		// }
-	}
-	else if((np->fheader).priority == 2)
-	{
-		full = enqueue(priority2, &f) * 2;
-		//if(debug)
-		// {
-		// 	printf("Put packet in Queue 2\n");
-		// 	printf("Queue 2 size : %d\n", priority2->size);
-		// } 
-	}
-	else if((np->fheader).priority == 3)
-	{
-		full = enqueue(priority3, &f) * 3;
-		//if(debug) 
-		// {
-		// 	printf("Put packet in Queue 3\n");
-		// printf("Queue 3 size : %d\n", priority3->size);
-			
-		// }
-	}
-	else
-	{
-		die("Error: Received packet with invalid priority\n");
-	}
-	//if(debug) printf("FORWARDING ENTRY:\nNEXT HOP: %d\nNEXT PORT: %d\n", f.host_addr, f.host_addr);
-
 	//packet dropped if queue is full, log incident
-	if(full < 0)
+	if((full = enqueue(queue, &f)) < 0)
 	{
 		printf("Packet not put in queue; it was full\n");
 		char errmsg [MAX_LINE];
 		sprintf(errmsg, "Queue %d is full. Packet will dropped.\n", full * -1);
-		logerror(*np, errmsg);
 		return -1;
 	}
 
@@ -411,21 +338,6 @@ void delay(double delay_time)
 	if(debug) printf("Done delaying for %fms\n", delay_time);
 }
 
-//randomly returns 1 or 0 based of probability of packet loss
-int drop(int probability)
-{
-	int this_prob = rand() % 101;
-
-	if(this_prob < probability)
-	{
-		if(debug) printf("Random value is %d; dropping packet\n", this_prob);
-		return 0;
-	}
-
-	if(debug) printf("Random value is %d; NOT dropping packet\n", this_prob);	
-	return 1;
-}
-
 //return -1 if f is NOT being delayed, 0 otherwise
 int packet_being_delayed(forward_info_t* f)
 {
@@ -451,17 +363,9 @@ int get_and_delay_packet(forward_info_t* f)
 {
 
 	//select packet from highest priority queue with packets
-	if(is_empty(priority1) != 0)
+	if(is_empty(queue) != 0)
 	{
-		dequeue(priority1, f);
-	}
-	else if(is_empty(priority2) != 0)
-	{
-		dequeue(priority2, f);
-	}
-	else if(is_empty(priority3) != 0)
-	{
-		dequeue(priority3, f);
+		dequeue(queue, f);
 	}
 	else //if no packets anywhere
 	{
@@ -474,7 +378,6 @@ int get_and_delay_packet(forward_info_t* f)
 	return 0;
 }
 
-
 //send a packet
 void send_packet(int s, forward_info_t* f)
 {
@@ -482,18 +385,6 @@ void send_packet(int s, forward_info_t* f)
 	//delay(f.delay);
 	fpacket_t np = (f->packet);
 
-	//randomly determine if packet should be dropped if its not end packet
-
-	if((f->packet).fpayload.header.type !='E' && (f->packet).fpayload.header.type !='R')
-	{
-		if(drop(f->prob_drop) != 1)
-		{
-			logerror(np, "Packet randomly dropped");
-			return;
-		}
-	}
-
-	//if(debug) printf("Create host data structure\n");
 	//create address structure 
 	struct sockaddr_in host;
 	host.sin_family	= AF_INET;
@@ -551,12 +442,11 @@ void route(fpacket_t packet)
 	if (destFound == 0)
 	{
 		if(debug)	printf("did not find next hop, going to log\n");
-		logerror(packet, "No forwarding entry found.");
 		return;
 	}
 
 	// We found a match, queue the packet for sending
-	queue(packet, forwardingEntry);
+	queue_packet(packet, forwardingEntry);
 }
 
 
@@ -585,12 +475,13 @@ int main(int argc, char* argv[])
 	//--------------------------INIT -----------------------------------
 	checkArgs(argc, argv);
 
-	//create queues
-	priority1 = init_queue(QUEUE_SIZE);
-	priority2 = init_queue(QUEUE_SIZE);
-	priority3 = init_queue(QUEUE_SIZE);
+	//create queue
+	queue = init_queue(QUEUE_SIZE);
 
-	buildTable();
+	//read topology table
+	read_topology();
+	calc_shortest_path();
+
 	//-----------------------SOCKET OPENING-----------------------------
 	//prepare emulator addr struct
 	struct sockaddr_in emulator, host;
@@ -659,12 +550,10 @@ int main(int argc, char* argv[])
 				has_curr_packet = false;
 			}
 		}
-
 		//get next packet to delay
 		//if no packet in queues, go wait for more packets to arrive
 		else if(get_and_delay_packet(&curr_delayed_packet) >= 0)
 		{
-			printf("Size of queues %d %d %d\n", priority1->size, priority2->size, priority3->size);
 			if(debug) printf("Started delaying a packet at ");
 			print_curr_time();
 			has_curr_packet = true;
